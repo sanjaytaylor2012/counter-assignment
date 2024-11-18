954f170bb202f7561e46ab13e573971c03265107
sanjaytaylor2012 <66386109+sanjaytaylor2012@users.noreply.github.com>
2024-11-18 13:29:20 -0500

    adding tests

--- src/tests/Counter.test.js
+++ src/tests/Counter.test.js
@@ -1,22 +1,40 @@
 // import necessary react testing library helpers here
+import { render, screen, waitFor  } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
 // import the Counter component here
+
+import Counter from "../components/Counter";
 
 beforeEach(() => {
   // Render the Counter component here
+  render(<Counter />);
+
 })
 
 test('renders counter message', () => {
-  // Complete the unit test below based on the objective in the line above
+  const counterMessage = screen.getByText(/Counter/i);
+  expect(counterMessage).toBeInTheDocument();
+
 });
 
 test('should render initial count with value of 0', () => {
-  // Complete the unit test below based on the objective in the line above
+  const countValue = screen.getByTestId('count');
+  expect(countValue).toHaveTextContent('0');
 });
 
-test('clicking + increments the count', () => {
-  // Complete the unit test below based on the objective in the line above
+test('clicking + increments the count', async () => {
+  const count = screen.getByTestId('count');
+  const incrementButton = screen.getByText('+');
+  userEvent.click(incrementButton);
+  await waitFor(() => expect(count).toHaveTextContent('1'));
+
 });
 
-test('clicking - decrements the count', () => {
-  // Complete the unit test below based on the objective in the line above
+test('clicking - decrements the count', async () => {
+  const count = screen.getByTestId('count');
+  const decrementButton = screen.getByText('-');
+  
+  userEvent.click(decrementButton);
+  await waitFor(() => expect(count).toHaveTextContent('-1'));
 });
